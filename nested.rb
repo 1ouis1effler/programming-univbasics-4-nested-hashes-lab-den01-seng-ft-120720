db0a05aa0f56569f233a5becb3b31e7f04e0df21
Louis Leffler <louisleffler@gmail.com>
2020-11-25 21:32:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,10 +84,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
-programmer_hash[:yukihiro_matsumoto] = {:known_for=>"Ruby",:languages=>["LISP", "C"]}
-programmer_hash
-end
+end 
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
